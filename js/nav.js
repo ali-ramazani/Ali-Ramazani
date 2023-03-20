9e8f8cf9e9d1a73a0dceb950a936f8e5992a4a50
Ali Ramazani <ramazanim@berea.edu>
2023-03-20 18:40:11 -0400

    March 20, 2023

--- js/nav.js
+++ js/nav.js
@@ -14,3 +14,16 @@ $(document).on("scroll", function(){
         $(".navbar-btn").removeClass("nav-btn-icon");
     }
 });
+
+
+
+$(document).ready(function(){
+  $(".work-filter-list").isotope();
+
+$(".work-filter li").on('click', function(){
+    var selector = $(this).attr('data-filter');
+    $(".work-filter-list").isotope({
+        filter:selector
+    });
+});
+})
